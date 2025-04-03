571d79abe918c9da3fc1133611cc03eb71443ddf
Martin Löper <martin.loeper@nesto-software.de>
2025-04-03 22:07:59 +0200

    feat: add simple hm-module for ouch

---
+++ plugins/ouch/hm-module.nix
@@ -0,0 +1,14 @@
+{
+  options = { pkgs, mkKeyOption, ... }:
+    _: {
+      keys = {
+        compress = mkKeyOption {
+          on = [ "C" ];
+          run = "plugin ouch";
+          desc = "Compress with ouch";
+        };
+      };
+      runtimeDeps = [ pkgs.ouch ];
+    };
+  config = { cfg, setKeys, ... }: _: (setKeys cfg.keys);
+}
