da9698c9e90cfb8636a3835c0b0750019def8b9e
Martin Löper <martin.loeper@nesto-software.de>
2025-04-03 22:41:38 +0200

    fix

--- plugins/ouch/package.nix
+++ plugins/ouch/package.nix
@@ -1,17 +1,13 @@
-{
-  lib,
-  stdenv,
-  fetchFromGitHub,
-}:
+{ lib, stdenv, fetchFromGitHub, }:
 
 stdenv.mkDerivation {
   pname = "yaziPlugins-ouch";
-  version = "unstable-2025-02-18";
+  version = "unstable-2025-04-03";
 
   src = fetchFromGitHub {
     owner = "ndtoan96";
     repo = "ouch.yazi";
-    rev = "ce6fb75431b9d0d88efc6ae92e8a8ebb9bc1864a";
+    rev = "558188d2479d73cafb7ad8fb1bee12b2b59fb607";
     hash = "sha256-oUEUGgeVbljQICB43v9DeEM3XWMAKt3Ll11IcLCS/PA=";
   };
 
